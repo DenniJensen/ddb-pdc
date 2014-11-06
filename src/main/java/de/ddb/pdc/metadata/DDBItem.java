a74fcc71abdce0881a647eac590f7aeedc35c690
LichtiNam <goerickm@zedat.fu-berlin.de>
2014-11-06 22:55:56 +0100

    DDBItem new Object only for search result, for other metadata input later

--- src/main/java/de/ddb/pdc/metadata/DDBItem.java
+++ src/main/java/de/ddb/pdc/metadata/DDBItem.java
@@ -7,5 +7,51 @@ public class DDBItem {
   private String id;
   private String author;
   private String title;
-  // ...
+  private String subtitle;
+  private String imgUrl;
+  
+  public DDBItem(String id) {
+	  this.id = id;
+  }
+
+  public String getId() {
+	return id;
+  }
+
+  public void setId(String id) {
+	this.id = id;
+  }
+
+  public String getAuthor() {
+	return author;
+  }
+
+  public void setAuthor(String author) {
+	this.author = author;
+  }
+
+  public String getTitle() {
+	return title;
+  }
+
+  public void setTitle(String title) {
+	this.title = title;
+  }
+
+  public String getSubtitle() {
+	return subtitle;
+  }
+
+  public void setSubtitle(String subtitle) {
+	this.subtitle = subtitle;
+  }
+
+  public String getImgUrl() {
+	return imgUrl;
+  }
+
+  public void setImgUrl(String imgUrl) {
+	this.imgUrl = imgUrl;
+  }
+  
 }
