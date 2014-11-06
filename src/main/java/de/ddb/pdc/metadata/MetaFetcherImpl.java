7cc22a043e7db65acda79eef51beb855a9c84dfe
LichtiNam <goerickm@zedat.fu-berlin.de>
2014-11-06 23:56:41 +0100

    delete stylecheck error

---
+++ src/main/java/de/ddb/pdc/metadata/MetaFetcherImpl.java
@@ -0,0 +1,79 @@
+package de.ddb.pdc.metadata;
+
+import java.util.ArrayList;
+import java.util.LinkedHashMap;
+
+import org.springframework.web.client.RestTemplate;
+/**
+ * 
+ * Implementation of a {@link MetaFetcher}
+ *
+ */
+public class MetaFetcherImpl implements MetaFetcher {
+
+  private static final String URL = 
+    "https://www.deutsche-digitale-bibliothek.de";
+  private static final String APIURL = 
+    "https://api.deutsche-digitale-bibliothek.de";
+  private static final String SEARCH = "/search?";
+  private static final String ITEMS = "/items/";
+  private static final String AUTH = "oauth_consumer_key=";
+  private static final String SORTROWS = "&sort=RELEVANCE&rows=";
+  private static final String QUERY = "&query=";
+  private static final String EDM = "/edm?";
+
+  private String authKey = "";
+
+  /** 
+   * @param authKey authentication Key for the DDB API
+   */
+  public MetaFetcherImpl(String authKey) {
+    this.authKey = authKey;
+  }
+
+  /**
+   * @param query 
+   * @param maxCount
+   * @return array of DDBItems with the search result
+   */
+  public DDBItem[] getSearchResult(String query, int maxCount) {
+    RestTemplate restTemplate = new RestTemplate();
+    String modifiedQuery = query.replace(" ", "+");
+    String url = APIURL + SEARCH + AUTH + authKey + SORTROWS + maxCount + QUERY
+      + modifiedQuery;
+    JSONResult jsonResult = restTemplate.getForObject(url, JSONResult.class);
+    return getDDBItems(jsonResult);
+  }
+
+
+  /**
+   * @param ddbItem for the id to get the meta data
+   */
+  public void getMetaData(DDBItem ddbItem) {
+
+  }
+
+  /**
+   * @param jsonresult
+   * @return array filled with search results
+   */
+  @SuppressWarnings({ "rawtypes", "unchecked" })
+  private DDBItem[] getDDBItems(JSONResult jsonresult) {
+    ArrayList<LinkedHashMap> results = (ArrayList<LinkedHashMap>) jsonresult
+      .getResults().get(0).get("docs");
+    int maxResults = results.size();
+    DDBItem[] ddbItems = new DDBItem[maxResults];
+    
+    for (int idx = 0; idx < maxResults; idx++) {
+      DDBItem ddbItem = new DDBItem((String) results.get(idx).get("id"));
+      ddbItem.setTitle(MetaFetcherUtil.deleteMatch((String) results.get(idx)
+        .get("title")));
+      ddbItem.setSubtitle(MetaFetcherUtil.deleteMatch((String) results
+        .get(idx).get("subtitle")));
+      ddbItem.setImgUrl(URL + (String) results.get(idx).get("thumbnail"));
+      ddbItems[idx] = ddbItem;
+    }
+
+    return ddbItems;
+  }
+}
