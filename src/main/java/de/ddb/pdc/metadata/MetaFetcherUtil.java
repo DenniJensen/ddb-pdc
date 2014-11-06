13843a80837aad9a5764f206c1d89abb15855f48
LichtiNam <goerickm@zedat.fu-berlin.de>
2014-11-06 22:59:22 +0100

    Utility class for the Metafetcher

---
+++ src/main/java/de/ddb/pdc/metadata/MetaFetcherUtil.java
@@ -0,0 +1,17 @@
+package de.ddb.pdc.metadata;
+
+/**
+ * 
+ * utility function for the MetaFetcher
+ *
+ */
+public class MetaFetcherUtil {
+
+  /**
+   * @param string
+   * @return string with replaced match string
+   */
+  public static String deleteMatch(String string) {
+	return string.replace("<match>", "").replace("</match>", "");
+  }
+}
