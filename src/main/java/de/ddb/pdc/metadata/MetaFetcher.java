f2b5bf1cc5fad95697113baf828a62d19d75c084
LichtiNam <goerickm@zedat.fu-berlin.de>
2014-11-07 21:18:18 +0100

    add throws and java-doc

--- src/main/java/de/ddb/pdc/metadata/MetaFetcher.java
+++ src/main/java/de/ddb/pdc/metadata/MetaFetcher.java
@@ -1,7 +1,23 @@
 package de.ddb.pdc.metadata;
 
+import org.springframework.web.client.RestClientException;
+
+/**
+ * Interface of the MetaFetcher
+ */
 public interface MetaFetcher {
 
-  public DDBItem[] getSearchResult(String query, int maxCount);
-  public void getMetaData(DDBItem ddbItem);
+  /**
+   * @param query what to search
+   * @param maxCount how many items will be search
+   * @return Array of DDBItems with search result
+   * @throws RestClientException
+   */
+  public DDBItem[] searchForItems(String query, int maxCount) throws RestClientException;
+  
+  /**
+   * @param ddbItem used to get metadata with the id of the DDBItem
+   * @throws RestClientException
+   */
+  public void fetchMetadata(DDBItem ddbItem) throws RestClientException;
 }
