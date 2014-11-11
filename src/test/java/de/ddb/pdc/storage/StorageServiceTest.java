a14ea4b0cc11fed72215c2b7f85bb1bf856febed
Philipp Decurtins <pdecurtins@zedat.fu-berlin.de>
2014-11-11 01:21:22 +0100

    Added fetch test case

--- src/test/java/de/ddb/pdc/storage/StorageServiceTest.java
+++ src/test/java/de/ddb/pdc/storage/StorageServiceTest.java
@@ -1,10 +1,17 @@
 /** 
  * This is not a unit test, this is a real test. :D
+ * Tests the store and fetch methods 
+ * Test behaviour is specified by the included Configuration class and
+ * environment properties are loaded from a designated test file.
+ * TODO Make the Configuration class available to other test classes.
+ * TODO Implement the remaining test case for the update method.
  */
 package de.ddb.pdc.storage;
 
 import com.mongodb.MongoClient;
 import java.net.UnknownHostException;
+import java.util.Date;
+import org.junit.BeforeClass;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -13,6 +20,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
+import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
 import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
@@ -26,7 +34,7 @@ import org.springframework.test.context.support.AnnotationConfigContextLoader;
 public class StorageServiceTest {
   
   private StorageService storageService;
-    
+  
   public StorageServiceTest() {
   }
   
@@ -34,23 +42,16 @@ public class StorageServiceTest {
   public void setStorageService(StorageService storageService) {
     this.storageService = storageService;
   }
-
+  
   /**
    * Test of store method, of class StorageService.
    */
   @Test
   public void testStore() {
     MongoDataModel mdm = new MongoDataModel(
-        "1234","movies","institute",true,null,"2014-11-10"
+        "1234","movies","testMAYNE",true,null,new Date().toString()
     );
     storageService.store(mdm);
-  }
-
-  /**
-   * Test of update method, of class StorageService.
-   */
-  @Test
-  public void testUpdate() {
   }
   
   /**
@@ -58,40 +59,53 @@ public class StorageServiceTest {
    */
   @Test
   public void testFetch() {
+    MongoDataModel mdm = storageService.fetch("1234");
+    
   }
 
-  /**
-   * Test of fetchAll method, of class StorageService.
-   */
-  @Test
-  public void testFetchAll() {
-  }
-
-  /**
-   * Test of deleteAll method, of class StorageService.
-   */
-  @Test
-  public void testDeleteAll() {
-  }
-  
   /**
    * Configuration for creating the required Beans needed by the test class.
-   * 
+   * Environment configurations are loaded from a designated test file.
    * TODO Clarify why the configuration class must be static.
    */
   @Configuration
-  @ComponentScan  
+  @ComponentScan
+  @PropertySource("classpath:test.application.properties")
   public static class TestConfiguration {
     
+    @Value("${host.ip:127.0.0.1}")
+    private String hostIp;
+    
+    @Value("${host.port:27017}")
+    private int hostPort;
+    
+    @Value("${database:pdcTest}")
+    private String database;
+    
+    @Value("${collection.name:pdcDataTest}")
+    private String collectionName;
+           
     @Bean
     public MongoTemplate mongoTemplate() throws UnknownHostException {
-      return new MongoTemplate(new MongoClient("localhost", 27017), "pdc");
+      return new MongoTemplate(new MongoClient(hostIp, hostPort), database);
     }
     
     @Bean
     public StorageService storageService() throws UnknownHostException {
-      return new MongoStorageServiceImpl(mongoTemplate(), "pdcData");
+      return new MongoStorageServiceImpl(mongoTemplate(), collectionName);
     }
+    
+    /**
+     * Refer to Spring documentation for Annotation Type PropertySource.
+     * TODO change configuration class to avoid @Value as described in
+     * @see <a href="https://jira.spring.io/browse/SPR-8539">Spring.io</a>.
+     * @return something really long
+     */
+    @Bean
+    public static PropertySourcesPlaceholderConfigurer propertyPlaceholderConfigurer() {
+      return new PropertySourcesPlaceholderConfigurer();
+    }
+    
   }
   
 }
