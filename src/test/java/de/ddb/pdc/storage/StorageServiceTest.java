31f95f45d25ea4aab34e13235b3644d9710325cc
alexd <alxbfk88@gmail.com>
2014-11-16 19:01:09 +0100

    Add Test-Methods for testing MongoDB

--- src/test/java/de/ddb/pdc/storage/StorageServiceTest.java
+++ src/test/java/de/ddb/pdc/storage/StorageServiceTest.java
@@ -1,6 +1,6 @@
-/** 
+/**
  * This is not a unit test, this is a real test. :D
- * Tests the store and fetch methods 
+ * Tests the store and fetch methods
  * Test behaviour is specified by the included Configuration class and
  * environment properties are loaded from a designated test file.
  * TODO Make the Configuration class available to other test classes.
@@ -11,7 +11,11 @@ package de.ddb.pdc.storage;
 
 import com.mongodb.MongoClient;
 import java.net.UnknownHostException;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
+import org.junit.Assert;
+import org.junit.Ignore;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -32,35 +36,84 @@ import org.springframework.test.context.support.AnnotationConfigContextLoader;
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(loader = AnnotationConfigContextLoader.class)
 public class StorageServiceTest {
-  
+
   private StorageService storageService;
-  
+
   public StorageServiceTest() {
   }
-  
+
   @Autowired
   public void setStorageService(StorageService storageService) {
     this.storageService = storageService;
   }
-  
+
   /**
-   * Test of store method, of class StorageService.
+   * Test of store and fetch method, of class StorageService.
+   * ItemID have to be an unique number.
    */
   @Test
-  public void testStore() {
-    MongoDataModel mdm = new MongoDataModel(
-        "1234","movies","testMAYNE",true,null,new Date().toString()
-    );
-    storageService.store(mdm);
+  public void testStoreAndFetch() {
+    /* Dummy data */
+    final String itemID = "156987";
+    final String itemCategory = "Movies";
+    final String institute = "Insti";
+    final boolean publicDomain = false;
+    final List<String> trace = new ArrayList<String>();
+    trace.add("Question A");
+    trace.add("Question B");
+    trace.add("Question C");
+    final String timeStamp = new Date().toString();
+    /* -------- */
+
+    // create and store a new entry in DB
+    MongoDataModel newEntry = new MongoDataModel(
+        itemID,itemCategory,institute,publicDomain,trace,timeStamp);
+
+    storageService.store(newEntry);
+
+    // fetch stored entry
+    MongoDataModel storedEntry = storageService.fetch(itemID);
+
+    boolean check = compareTwoEntries(newEntry, storedEntry);
+
+    Assert.assertEquals(true, check);
   }
-  
+
   /**
-   * Test of fetch method, of class StorageService.
+   * Compares two Entries and return true if these have the same values
+   *
+   * @param mdm1
+   * @param mdm2
+   *
+   * @return true if entries are equal
+   */
+  private boolean compareTwoEntries(MongoDataModel mdm1, MongoDataModel mdm2){
+    boolean equal = false;
+
+    if((mdm1.getItemId().equals(mdm2.getItemId())) &&
+            (mdm1.getItemCategory().equals(mdm2.getItemCategory())) &&
+            (mdm1.getInstitute().equals(mdm2.getInstitute())) &&
+            (mdm1.isPublicDomain() == mdm2.isPublicDomain())&&
+            (mdm1.getTrace().equals(mdm2.getTrace())) &&
+            (mdm1.getTimestampAsString().equals(mdm2.getTimestampAsString()))
+            ){
+      equal = true;
+    }
+    return equal;
+  }
+
+  /**
+   * Test of deleteAll-method of class StorageService.
+   *
    */
   @Test
-  public void testFetch() {
-    MongoDataModel mdm = storageService.fetch("1234");
-    
+  public void testDeleteAll() {
+    List <MongoDataModel> entriesBefore= storageService.fetchAll();
+    Assert.assertEquals(false, entriesBefore.isEmpty());
+    storageService.deleteAll();
+    List <MongoDataModel> entriesAfter = storageService.fetchAll();
+    Assert.assertEquals(true, entriesAfter.isEmpty());
+
   }
 
   /**
@@ -72,29 +125,29 @@ public class StorageServiceTest {
   @ComponentScan
   @PropertySource("classpath:test.application.properties")
   public static class TestConfiguration {
-    
+
     @Value("${host.ip:127.0.0.1}")
     private String hostIp;
-    
+
     @Value("${host.port:27017}")
     private int hostPort;
-    
+
     @Value("${database:pdcTest}")
     private String database;
-    
+
     @Value("${collection.name:pdcDataTest}")
     private String collectionName;
-           
+
     @Bean
     public MongoTemplate mongoTemplate() throws UnknownHostException {
       return new MongoTemplate(new MongoClient(hostIp, hostPort), database);
     }
-    
+
     @Bean
     public StorageService storageService() throws UnknownHostException {
       return new MongoStorageServiceImpl(mongoTemplate(), collectionName);
     }
-    
+
     /**
      * Refer to Spring documentation for Annotation Type PropertySource.
      * TODO change configuration class to avoid @Value as described in
@@ -105,7 +158,7 @@ public class StorageServiceTest {
     public static PropertySourcesPlaceholderConfigurer propertyPlaceholderConfigurer() {
       return new PropertySourcesPlaceholderConfigurer();
     }
-    
+
   }
-  
+
 }
