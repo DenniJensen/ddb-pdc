27717f8523a2c15000c792fc0b5ee9f175b2a405
alexd <alxbfk88@gmail.com>
2014-11-23 22:13:06 +0100

    Add test for calculate-method in PDCControllerTest.

--- src/test/java/de/ddb/pdc/web/PDCControllerTest.java
+++ src/test/java/de/ddb/pdc/web/PDCControllerTest.java
@@ -18,15 +18,18 @@ public class PDCControllerTest {
 
   @Test
   public void calculate() throws Exception{
+
     MetaFetcher mfetcher = Mockito.mock(MetaFetcher.class);
-    AnswererService ansService = Mockito.mock(AnswererService.class);
     DDBItem ddbItemfromMetaFetcher = new DDBItem("123");
+    Mockito.when(mfetcher.fetchMetadata("123")).thenReturn(ddbItemfromMetaFetcher);
+
+    AnswererService ansService = Mockito.mock(AnswererService.class);
     List<AnsweredQuestion> trace = new ArrayList<AnsweredQuestion>();
     PDCResult pdcResult = new PDCResult(true,trace);
-    Mockito.when(mfetcher.fetchMetadata("123")).thenReturn(ddbItemfromMetaFetcher);
-    Mockito.when(ansService.getResult("Germany", ddbItemfromMetaFetcher)).thenReturn(pdcResult);
+    // country is null
+    Mockito.when(ansService.getResult(null, mfetcher.fetchMetadata("123"))).thenReturn(pdcResult);
+
     PDCController pdcController = new PDCController(mfetcher, ansService);
-    Mockito.when(pdcController.calculate("123")).thenReturn(pdcResult);
 
     Assert.assertSame(pdcResult, pdcController.calculate("123"));
   }
