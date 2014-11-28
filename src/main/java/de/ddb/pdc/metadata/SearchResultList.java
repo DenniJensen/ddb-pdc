1ac7cdb9eed551a9141a6547f3161a43ada8b6c6
LichtiNam <goerickm@zedat.fu-berlin.de>
2014-11-29 00:44:35 +0100

    add documentation

--- src/main/java/de/ddb/pdc/metadata/SearchResultList.java
+++ src/main/java/de/ddb/pdc/metadata/SearchResultList.java
@@ -3,11 +3,18 @@ package de.ddb.pdc.metadata;
 import java.util.ArrayList;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
+/**
+ * class to list the SearchResultItems
+ */
 @JsonIgnoreProperties(ignoreUnknown = true)
 class SearchResultList {
 
   private ArrayList<SearchResultItem> docs;
 
+  /**
+   * Returns a list with all docs of the result item from the search
+   * json result.
+   */
   public ArrayList<SearchResultItem> getDocs() {
     return docs;
   }
