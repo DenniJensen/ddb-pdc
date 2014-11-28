ea02277f6fbdcd0cd6889951f1bb5ea8d4679735
LichtiNam <goerickm@zedat.fu-berlin.de>
2014-11-29 00:41:10 +0100

    add documentation and change getInstitution to the right value from aggregation dataProvider

--- src/main/java/de/ddb/pdc/metadata/RDFItem.java
+++ src/main/java/de/ddb/pdc/metadata/RDFItem.java
@@ -1,12 +1,16 @@
 package de.ddb.pdc.metadata;
 
 import java.util.ArrayList;
+import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
+/**
+ * class for rdf item from edm item of aip json request
+ */
 @JsonIgnoreProperties(ignoreUnknown = true)
 class RDFItem {
 
@@ -19,6 +23,9 @@ class RDFItem {
   @JsonProperty("Aggregation")
   private Map<String, Object> aggregation;
 
+  /**
+   * @return a 4 digit int if a issued year is found or -1
+   */
   public int getPublishYear() {
     try {
       String publishedYear = (String) providedCHO.get("issued");
@@ -26,16 +33,25 @@ class RDFItem {
         // The item is missing the publishing date.
         return -1;
       }
-      return Integer.parseInt(publishedYear.split(",")[0]);
+
+      return Integer.parseInt(Utility.useRegex(publishedYear,"\\d{4}"));
     } catch (NumberFormatException e) {
-      throw new RuntimeException(e);
+      return -1;
     }
   }
 
+  /**
+   * @return  institution from the dataProvider item of aggregation
+   */
   public String getInstitution() {
-    return (String) aggregation.get("provider");
+    List<Object> dataProvider = (ArrayList<Object>) aggregation
+      .get("dataProvider");
+    return dataProvider.get(0).toString();
   }
 
+  /**
+   * @return  list of all author ids
+   */
   public List<String> getAuthorIds() {
     List<String> authorIds = new ArrayList<>();
     for (Map agent : getAgents()) {
@@ -47,6 +63,9 @@ class RDFItem {
     return authorIds;
   }
 
+  /**
+   * @return  list of all agents item in rdf item
+   */
   private List<Map> getAgents() {
     if (agents instanceof List) {
       // Multiple agents
