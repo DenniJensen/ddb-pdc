9ef1f564c8d37c639aaf0a38a28da785eb1c3855
LichtiNam <goerickm@zedat.fu-berlin.de>
2014-11-29 00:38:56 +0100

    add documentation and null check

--- src/main/java/de/ddb/pdc/metadata/ItemAipResult.java
+++ src/main/java/de/ddb/pdc/metadata/ItemAipResult.java
@@ -4,6 +4,9 @@ import com.fasterxml.jackson.annotation.JsonFormat;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
+/**
+ * call for the aip json result
+ */
 @JsonFormat
 @JsonIgnoreProperties(ignoreUnknown = true)
 class ItemAipResult {
@@ -11,7 +14,13 @@ class ItemAipResult {
   @JsonProperty
   private EDMItem edm;
 
+  /**
+   * @return rdf item of the json result or null
+   */
   public RDFItem getRDFItem() {
+    if (edm == null) {
+      return null;
+    }
     return edm.getRdf();
   }
 }
