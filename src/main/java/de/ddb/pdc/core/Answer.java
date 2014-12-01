b2f9fbc0c1431b2f8bb1bb6a83e34ddd806fca57
Frank Zechert <admin@ch3ck3r.org>
2014-12-01 18:58:14 +0100

    removed multiple @JsonValue annotated methods. These cause an error in Jackson

--- src/main/java/de/ddb/pdc/core/Answer.java
+++ src/main/java/de/ddb/pdc/core/Answer.java
@@ -39,24 +39,6 @@ public enum Answer {
   UNKNOWN;
 
   /**
-   * Converts the answer to an integer used for the json serialization.
-   * NO = 0, YES = 1, ASSUMED_NO = 2, ASSUMED_YES = 3, UNKNOWN = 4
-   *
-   * @return 0 if the answer is no, 1 if the answer is yes, 2 if the answer
-   *         is assumed no, 3 if the answer is assumed yes, and 4 if the answer
-   *         is unknown
-   */
-  @JsonValue
-  public int toInteger() {
-    for (int index = 0; index < Answer.values().length; index++) {
-      if (Answer.values()[index] == this) {
-        return index;
-      }
-    }
-    return -1; // this should never happen
-  }
-
-  /**
    * Converts the answer into a string for the json serialization.
    * NO = "no", YES = "yes", ASSUMED_NO = "assumed no",
    * ASSUMED_YES = "assumed yes", UNKNOWN = "unknown"
