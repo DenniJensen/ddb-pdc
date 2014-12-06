cba0fcba9bd487161ea1eac164c5ad4455242272
Denis Washington <denisw@online.de>
2014-12-06 20:21:46 +0100

    Factor out API URL prefix in DdbApiUrlsTest

--- src/test/java/de/ddb/pdc/metadata/DdbApiUrlsTest.java
+++ src/test/java/de/ddb/pdc/metadata/DdbApiUrlsTest.java
@@ -6,24 +6,27 @@ import static org.junit.Assert.assertEquals;
 
 public class DdbApiUrlsTest {
 
+  private static final String API_URL_PREFIX =
+      "https://api.deutsche-digitale-bibliothek.de";
+
   @Test
   public void searchUrl() {
-    assertEquals(
-        "https://api.deutsche-digitale-bibliothek.de/search?query=foo&rows=123&sort=relevance&oauth_consumer_key=key",
+    assertEquals(API_URL_PREFIX +
+        "/search?query=foo&rows=123&sort=relevance&oauth_consumer_key=key",
         DdbApiUrls.searchUrl("foo", 123, "key"));
   }
 
   @Test
   public void itemAipUrl() {
-    assertEquals(
-        "https://api.deutsche-digitale-bibliothek.de/items/abcde/aip?oauth_consumer_key=key",
+    assertEquals(API_URL_PREFIX +
+        "/items/abcde/aip?oauth_consumer_key=key",
         DdbApiUrls.itemAipUrl("abcde", "key"));
   }
 
   @Test
   public void entityUrl() {
-    assertEquals(
-        "https://api.deutsche-digitale-bibliothek.de/entities?query=id:\"abcde\"&oauth_consumer_key=key",
+    assertEquals(API_URL_PREFIX +
+        "/entities?query=id:\"abcde\"&oauth_consumer_key=key",
         DdbApiUrls.entityUrl("abcde", "key"));
   }
 }
