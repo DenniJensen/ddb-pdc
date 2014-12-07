0b5e86fa475c735da5613f3cf9b63e16d1fe1e1b
Frank Zechert <admin@ch3ck3r.org>
2014-12-07 16:00:10 +0100

    added a crawler to the project

---
+++ src/main/java/de/ddb/pdc/crawler/Crawler.java
@@ -0,0 +1,108 @@
+package de.ddb.pdc.crawler;
+
+import java.util.Arrays;
+import java.util.List;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+/**
+ * The crawler is a command line application that calculates the public domain
+ * status of DDBItems starting with a search term (for now '*'). It will
+ * log all errors that occur during calculation of these DDBItems.
+ */
+@Component
+@SuppressWarnings("nls")
+public class Crawler implements CommandLineRunner {
+
+  private static final Log LOG = LogFactory.getLog(Crawler.class);
+  private boolean enabled = false;
+  private int maxDepth = 1000;
+  private int fetchSize = 10;
+  private final int timeout = 1000;
+  private final CrawlerSchedule schedule;
+
+  @Autowired
+  public Crawler(CrawlerSchedule schedule) {
+    this.schedule = schedule;
+  }
+
+  @Override
+  public void run(String... args) throws Exception {
+    List<String> argsList = Arrays.asList(args);
+
+    enabled = this.isCommandLineArgument(argsList, "-c", "--crawler");
+    String maxDepthString = getCommandLineArgument(argsList, "--depth");
+    String fetchSizeString = getCommandLineArgument(argsList, "--fetchSize");
+
+    if (maxDepthString != null) {
+      try {
+        maxDepth = Integer.parseInt(maxDepthString);
+      } catch (NumberFormatException e) {
+        // do nothing, keep the default maxDepth
+      }
+    }
+
+    if (fetchSizeString != null) {
+      try {
+        fetchSize = Integer.parseInt(fetchSizeString);
+      } catch (NumberFormatException e) {
+        // do nothing, keep the default fetchSize
+      }
+    }
+
+    if (!enabled) {
+      LOG.info("The crawler is disabled. Enable it with --crawler or -c.");
+      return;
+    }
+
+    LOG.info(String.format("The crawler is enabled. Max depth: %d, fetch size: "
+        + "%d", maxDepth, fetchSize));
+
+    schedule.setTimeout(timeout);
+    schedule.setMaxDepth(maxDepth);
+    schedule.setFetchSize(fetchSize);
+    schedule.start();
+  }
+
+  /**
+   * Check if at least one of the arguments given in cmds is present in the
+   * command line arguments given at program start.
+   * @param args The command line arguments.
+   * @param cmds The command line switches to search for.
+   * @return true if at least one of cmds command line switches is present in
+   *         the arguments. false otherwise.
+   */
+  @SuppressWarnings("static-method")
+  private boolean isCommandLineArgument(List<String> args, String... cmds) {
+    for (String cmd : cmds) {
+      if (args.contains(cmd)) {
+        return true;
+      }
+    }
+    return false;
+  }
+
+  /**
+   * Search for a command line argument and return the value given to this
+   * parameter.
+   * @param args The command line arguments.
+   * @param cmd The commnd line argument to return the value for.
+   * @return The value as String or null if no value was found.
+   */
+  @SuppressWarnings("static-method")
+  private String getCommandLineArgument(List<String> args, String cmd) {
+    boolean found = false;
+    for (String arg : args) {
+      if (arg.equalsIgnoreCase(cmd)) {
+        found =  true;
+      } else if (found) {
+        return arg;
+      }
+    }
+    return null;
+  }
+}
---
+++ src/main/java/de/ddb/pdc/crawler/CrawlerSchedule.java
@@ -0,0 +1,144 @@
+package de.ddb.pdc.crawler;
+
+import java.util.LinkedList;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+import de.ddb.pdc.core.PDCResult;
+import de.ddb.pdc.core.PublicDomainCalculator;
+import de.ddb.pdc.metadata.DDBItem;
+import de.ddb.pdc.metadata.MetaFetcher;
+
+/**
+ * The crawler schedule does the actual crawling and calculating of DDBItems.
+ * It will schedule its actions according to the maxDepth, fetchSize and offset
+ * given to it.
+ */
+@Service
+@SuppressWarnings("nls")
+public class CrawlerSchedule extends Thread {
+
+  @Value("${ddb.country:de}")
+  private String country;
+
+  private static final Log LOG = LogFactory.getLog(CrawlerSchedule.class);
+  private int maxDepth;
+  private int fetchSize;
+  private int offset;
+  private long timeout;
+  private boolean end;
+  private final LinkedList<DDBItem> fetchedResults;
+  private final MetaFetcher metaFetcher;
+  private final PublicDomainCalculator calculator;
+
+  @Autowired
+  public CrawlerSchedule(MetaFetcher metaFetcher,
+      PublicDomainCalculator calculator) {
+    super("CrawlerSchedule");
+    this.metaFetcher = metaFetcher;
+    this.calculator = calculator;
+    this.fetchedResults = new LinkedList<DDBItem>();
+    this.offset = 0;
+    this.end = false;
+  }
+
+  /**
+   * Set the maximum crawling depth.
+   * @param maxDepth The maximum crawling depth.
+   */
+  public void setMaxDepth(int maxDepth) {
+    this.maxDepth = maxDepth;
+  }
+
+  /**
+   * Set the fetch size. This is the amount of items that are fetched
+   * in one fetch phase.
+   * @param fetchSize The fetch size.
+   */
+  public void setFetchSize(int fetchSize) {
+    this.fetchSize = fetchSize;
+  }
+
+  /**
+   * Set the timeout in milliseconds between the actions of the crawler.
+   * @param timeout The timeout in milliseconds.
+   */
+  public void setTimeout(int timeout) {
+    this.timeout = timeout;
+  }
+
+  @Override
+  public void run() {
+    while (!end) {
+      if (fetchedResults.isEmpty()) {
+        runFetchPhase();
+      } else {
+        runCalculatePhase();
+      }
+      Thread.yield();
+
+      try {
+        Thread.sleep(timeout);
+      } catch (InterruptedException e) {
+        // we were interrupted, cancel the crawler thread
+        end = true;
+      }
+    }
+    LOG.info("Crawler schedule complete.");
+  }
+
+  /**
+   * In the fetch phase new items will be fetched from the DDB-API using
+   * the searchForItems method of the controller.
+   */
+  private void runFetchPhase() {
+    if (offset >= maxDepth) {
+      end = true;
+      LOG.info("Maximum crawling depth was reached.");
+      return;
+    }
+
+    if (offset != 0) {
+      end = true;
+      LOG.warn("Shutting down crawler because paginated search is not possible "
+          + "yet");
+      // TODO: edit this when paginated search is possible
+      return;
+    }
+
+    LOG.info(String.format("Running fetch phase from item %d to item %d",
+        offset, offset + fetchSize));
+    try {
+      DDBItem[] results = metaFetcher.searchForItems("*", fetchSize);
+      for (DDBItem result : results) {
+        fetchedResults.add(result);
+      }
+    } catch (Throwable e) {
+      LOG.error("Error while fetching results", e);
+    }
+    offset += fetchSize;
+  }
+
+  /**
+   * In the calculate phase the public domain status of fetched items
+   * is calculated.
+   */
+  private void runCalculatePhase() {
+    DDBItem item = fetchedResults.pop();
+    String id = item.getId();
+
+    LOG.info(String.format("Running calculate phase for item %s", id));
+    try {
+      PDCResult result = calculator.calculate(country, item);
+      LOG.info(String.format("Calculation successful for %s. Result: %s",
+          id, result.isPublicDomain()));
+    } catch (Throwable e) {
+      LOG.error(String.format("Calculation unsuccessful for %s.", id));
+      LOG.error("Here is the trace:", e);
+    }
+  }
+}
