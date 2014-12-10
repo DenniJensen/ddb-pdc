06a9635721f9a944bb2027b314d21412a92f3116
LichtiNam <goerickm@zedat.fu-berlin.de>
2014-12-10 14:31:44 +0100

    null check

--- src/main/java/de/ddb/pdc/metadata/EntitiesResult.java
+++ src/main/java/de/ddb/pdc/metadata/EntitiesResult.java
@@ -13,7 +13,7 @@ class EntitiesResult {
 
   public EntitiesResultItem getResultItem() {
 
-    if (results.size() == 0 || results.get(0).getDocs().size() == 0) {
+    if (results == null || results.size() == 0 || results.get(0).getDocs().size() == 0) {
       return null;
     } else {
       return results.get(0).getDocs().get(0);
