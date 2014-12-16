7c181dd97134b7fe4e64d28908ebedcd8fde5a7d
alexd <alxbfk88@gmail.com>
2014-12-16 10:55:43 +0100

    Make changes based on Review

--- src/test/java/de/ddb/pdc/core/QuestionnaireTest.java
+++ src/test/java/de/ddb/pdc/core/QuestionnaireTest.java
@@ -1,14 +1,16 @@
 package de.ddb.pdc.core;
 
+import java.util.ArrayList;
+import java.util.List;
 import org.junit.Test;
 import static org.junit.Assert.*;
 
 public class QuestionnaireTest {
 
-@Test
-  public void literaryOrArtisticWorkFlowChart(){
+  @Test
+  public void literaryOrArtisticWorkFlowChart() {
     FlowChartStateGermany flowChartGermany =
-            FlowChartStateGermany.LITERATUR_OR_ARTISTIC_WORK;
+        FlowChartStateGermany.LITERATUR_OR_ARTISTIC_WORK;
 
     Questionnaire questionaireA = new Questionnaire(flowChartGermany);
     questionaireA.answerCurrentQuestion(Answer.NO, null);
@@ -34,12 +36,26 @@ public class QuestionnaireTest {
     questionaireC.answerCurrentQuestion(Answer.NO, null);
     questionaireC.answerCurrentQuestion(Answer.YES, null);
     assertTrue(questionaireC.isPublicDomain());
+
+    Questionnaire questionaireD = new Questionnaire(flowChartGermany);
+    questionaireD.answerCurrentQuestion(Answer.NO, null);
+    questionaireD.answerCurrentQuestion(Answer.NO, null);
+    questionaireD.answerCurrentQuestion(Answer.NO, null);
+    questionaireD.answerCurrentQuestion(Answer.NO, null);
+    questionaireD.answerCurrentQuestion(Answer.NO, null);
+    questionaireD.answerCurrentQuestion(Answer.NO, null);
+    questionaireD.answerCurrentQuestion(Answer.YES, null);
+    questionaireD.answerCurrentQuestion(Answer.YES, null);
+    questionaireD.answerCurrentQuestion(Answer.YES, null);
+    questionaireD.answerCurrentQuestion(Answer.YES, null);
+    questionaireD.answerCurrentQuestion(Answer.NO, null);
+    assertFalse(questionaireD.isPublicDomain());
   }
 
   @Test
-  public void nonOriginalPhotographFlowChart(){
+  public void nonOriginalPhotographFlowChart() {
     FlowChartStateGermany flowChartGermany =
-            FlowChartStateGermany.NON_ORIGINAL_PHOTOGRAPH;
+        FlowChartStateGermany.NON_ORIGINAL_PHOTOGRAPH;
 
     Questionnaire questionaireA = new Questionnaire(flowChartGermany);
     questionaireA.answerCurrentQuestion(Answer.YES, null);
@@ -53,9 +69,9 @@ public class QuestionnaireTest {
   }
 
   @Test
-  public void scientificEditionFlowChart(){
+  public void scientificEditionFlowChart() {
     FlowChartStateGermany flowChartGermany =
-            FlowChartStateGermany.SCIENTIFIC_EDITION_OF_OOC_WORK;
+        FlowChartStateGermany.SCIENTIFIC_EDITION_OF_OOC_WORK;
 
     Questionnaire questionaireA = new Questionnaire(flowChartGermany);
     questionaireA.answerCurrentQuestion(Answer.YES, null);
@@ -69,9 +85,9 @@ public class QuestionnaireTest {
   }
 
   @Test
-  public void phonogramFlowChart(){
+  public void phonogramFlowChart() {
     FlowChartStateGermany flowChartGermany =
-            FlowChartStateGermany.PHONOGRAM;
+        FlowChartStateGermany.PHONOGRAM;
 
     Questionnaire questionaireA = new Questionnaire(flowChartGermany);
     questionaireA.answerCurrentQuestion(Answer.YES, null);
@@ -86,7 +102,7 @@ public class QuestionnaireTest {
   }
 
   @Test
-  public void performanceFlowChart(){
+  public void performanceFlowChart() {
     FlowChartStateGermany flowChartGermany = FlowChartStateGermany.PERFORMANCE;
 
     Questionnaire questionaireA = new Questionnaire(flowChartGermany);
@@ -101,7 +117,7 @@ public class QuestionnaireTest {
   }
 
   @Test
-  public void unoriginalDatabaseFlowChart(){
+  public void unoriginalDatabaseFlowChart() {
     FlowChartStateGermany flowChartGermany = FlowChartStateGermany.DATABASE;
 
     Questionnaire questionaireA = new Questionnaire(flowChartGermany);
@@ -131,9 +147,9 @@ public class QuestionnaireTest {
   }
 
   @Test
-  public void firstFixationOfAFilmFlowChart(){
+  public void firstFixationOfAFilmFlowChart() {
     FlowChartStateGermany flowChartGermany =
-            FlowChartStateGermany.FIXATION_OF_FILM;
+        FlowChartStateGermany.FIXATION_OF_FILM;
 
     Questionnaire questionaireA = new Questionnaire(flowChartGermany);
     questionaireA.answerCurrentQuestion(Answer.YES, null);
@@ -145,5 +161,56 @@ public class QuestionnaireTest {
     questionaireB.answerCurrentQuestion(Answer.YES, null);
     assertTrue(questionaireB.isPublicDomain());
   }
+
+  @Test
+  public void checkPDCResult() {
+    FlowChartStateGermany flowChartGermany =
+        FlowChartStateGermany.LITERATUR_OR_ARTISTIC_WORK;
+
+    List<AnsweredQuestion> expectedTrace = new ArrayList<AnsweredQuestion>();
+    expectedTrace.add(new AnsweredQuestion(
+        Question.OFFICIAL_WORK_INTENDED_TO_BE_GENERALLY_RECEIVED,
+        Answer.NO, null));
+    expectedTrace.add(new AnsweredQuestion(
+        Question.COURT_DECISION_OR_DECISION_FORMULA, Answer.NO, null));
+    expectedTrace.add(new AnsweredQuestion(
+        Question.ACT_OF_PARLIAMENT, Answer.NO, null));
+    expectedTrace.add(new AnsweredQuestion(
+        Question.GOVERNMENT_DIRECTIVE, Answer.NO, null));
+    expectedTrace.add(new AnsweredQuestion(
+        Question.ANNOUNCEMENT_BY_AUTHORITY, Answer.NO, null));
+    expectedTrace.add(new AnsweredQuestion(
+        Question.AUTHOR_NATURAL_PERSON, Answer.YES, null));
+    expectedTrace.add(new AnsweredQuestion(
+        Question.AUTHOR_FROM_EUROPEAN_ECONOMIC_AREA, Answer.YES, null));
+    expectedTrace.add(new AnsweredQuestion(
+        Question.WORK_PUBLISHED_OR_COMMUNICATED, Answer.YES, null));
+    expectedTrace.add(new AnsweredQuestion(
+        Question.PUBLISHED_WITHIN_70_YEARS_OF_DEATH, Answer.YES, null));
+    expectedTrace.add(new AnsweredQuestion(
+        Question.AUTHOR_DIED_MORE_THAN_70_YEARS_AGO, Answer.NO, null));
+
+    Questionnaire questionaire = new Questionnaire(flowChartGermany);
+    questionaire.answerCurrentQuestion(Answer.NO, null);
+    questionaire.answerCurrentQuestion(Answer.NO, null);
+    questionaire.answerCurrentQuestion(Answer.NO, null);
+    questionaire.answerCurrentQuestion(Answer.NO, null);
+    questionaire.answerCurrentQuestion(Answer.NO, null);
+    questionaire.answerCurrentQuestion(Answer.YES, null);
+    questionaire.answerCurrentQuestion(Answer.YES, null);
+    questionaire.answerCurrentQuestion(Answer.YES, null);
+    questionaire.answerCurrentQuestion(Answer.YES, null);
+    questionaire.answerCurrentQuestion(Answer.NO, null);
+
+    for(int i = 0; i < questionaire.getTrace().size(); i++) {
+      assertTrue(expectedTrace.get(i).getQuestion().equals(
+          questionaire.getTrace().get(i).getQuestion()));
+      assertTrue(expectedTrace.get(i).getAnswer().equals(
+          questionaire.getTrace().get(i).getAnswer()));
+    }
+    assertFalse(questionaire.isPublicDomain());
+  }
+
+
 
 }
