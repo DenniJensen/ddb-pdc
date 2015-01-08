6e90bb932b7f3884a4383e1be9130e50a04808d2
LichtiNam <goerickm@zedat.fu-berlin.de>
2015-01-08 21:50:39 +0100

    adjust test but have problems

--- src/test/java/de/ddb/pdc/metadata/MetaFetcherImplTest.java
+++ src/test/java/de/ddb/pdc/metadata/MetaFetcherImplTest.java
@@ -3,24 +3,47 @@ package de.ddb.pdc.metadata;
 import org.junit.Before;
 import org.junit.Test;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.xml.xpath.Jaxp13XPathTemplate;
+import org.springframework.xml.xpath.XPathOperations;
+import org.w3c.dom.Node;
 
-import java.util.ArrayList;
-import java.util.Calendar;
-import java.util.GregorianCalendar;
-import java.util.List;
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.transform.dom.DOMSource;
+import java.util.*;
 
-import static org.junit.Assert.*;
-import static org.mockito.Mockito.*;
+import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 public class MetaFetcherImplTest {
 
   private MetaFetcherImpl fetcher;
   private RestTemplate rest;
+  private XPathOperations xpathTemplate;
+  private DOMSource domSource;
 
   @Before
   public void setUp() {
     rest = mock(RestTemplate.class);
     fetcher = new MetaFetcherImpl(rest, "authkey");
+
+    Map<String,String> namespaces = new HashMap<String,String>();
+    namespaces.put("rdf", "http://www.w3.org/1999/02/22-rdf-syntax-ns#");
+    namespaces.put("gndo", "http://d-nb.info/standards/elementset/gnd#");
+    namespaces.put("ctx", "http://www.deutsche-digitale-bibliothek.de/cortex");
+    namespaces.put("ns2", "http://www.deutsche-digitale-bibliothek.de/institution");
+    namespaces.put("ns3", "http://www.w3.org/1999/02/22-rdf-syntax-ns#");
+    namespaces.put("ns4", "http://www.deutsche-digitale-bibliothek.de/item");
+    namespaces.put("ore", "http://www.openarchives.org/ore/terms/");
+    namespaces.put("edm", "http://www.europeana.eu/schemas/edm/");
+    namespaces.put("skos", "http://www.w3.org/2004/02/skos/core#");
+    namespaces.put("dc", "http://purl.org/dc/elements/1.1/" );
+    namespaces.put("dcterms", "http://purl.org/dc/terms/");
+    Jaxp13XPathTemplate jaxp13XPathTemplate = new Jaxp13XPathTemplate();
+    jaxp13XPathTemplate.setNamespaces(namespaces);
+
+    xpathTemplate = mock(jaxp13XPathTemplate.getClass());
+    domSource = mock(DOMSource.class);
   }
 
   @Test
@@ -49,41 +72,45 @@ public class MetaFetcherImplTest {
   }
   
   @Test
-  public void fetch() {
-    DNBAuthorItem dnbA = mock(DNBAuthorItem.class);
-    when(dnbA.name()).thenReturn("Johann Wolfgang von Goethe");
-    when(dnbA.placeOfDeathUri()).thenReturn("http://d-nb.info/gnd/4062257-5");
-    Calendar calB = new GregorianCalendar(1749,14,5);
-    when(dnbA.dateOfBirth()).thenReturn(calB);
-    Calendar calD = new GregorianCalendar(1832,16,06);
-    when(dnbA.dateOfDeath()).thenReturn(calD);
+  public void fetch() throws ParserConfigurationException {
+    String xpathIssued = "//dcterms:issued";
+    String xpathInstitution = "//edm:dataProvider[1]";
+    String xpathName = "//gndo:variantNameForThePerson";
+    String xpathDOB  = "//gndo:dateOfBirth";
+    String xpathDOD  = "//gndo:dateOfDeath";
+    String xpathPOD  = "//gndo:placeOfDeath/rdf:Description/@rdf:about";
+    String xpathLoc  = "//gndo:geographicAreaCode/@rdf:resource";
+    String xpathFacetRole = "//ctx:facet[@name='affiliate_fct_role_normdata']/ctx:value";
+    String xpathFacet = "//ctx:facet[@name='affiliate_fct_normdata']/ctx:value";
+    when(xpathTemplate.evaluateAsString("//dcterms:issued", domSource)).thenReturn("1946");
+    when(xpathTemplate.evaluateAsString(xpathInstitution, domSource)).thenReturn("Deutsche Digitale Bibliothek");
 
-    DNBLocationItem dnbL = mock(DNBLocationItem.class);
-    when(dnbL.locate()).thenReturn("de");
-    
-    RDFItem rdf = mock(RDFItem.class);
-    String authorId = "http://d-nb.info/gnd/118540238";
-    List<String> authorIds = new ArrayList<>();
-    authorIds.add(authorId);
-    when(rdf.getAuthorIds()).thenReturn(authorIds);
-    when(rdf.getInstitution()).thenReturn("Deutsche Digitale Bibliothek");
-    when(rdf.getPublishYear()).thenReturn(1946);
-    
-    EDMItem edm = mock(EDMItem.class);
-    when(edm.getRdf()).thenReturn(rdf);
-    
-    ItemAipResult result = mock(ItemAipResult.class);
-    when(result.getRDFItem()).thenReturn(rdf);
+    when(xpathTemplate.evaluateAsString(xpathDOB, domSource)).thenReturn("1748-05-14");
+    when(xpathTemplate.evaluateAsString(xpathDOD, domSource)).thenReturn("1832-06-16");
+    when(xpathTemplate.evaluateAsString(xpathPOD, domSource)).thenReturn("http://d-nb.info/gnd/4062257-5");
+    when(xpathTemplate.evaluateAsString(xpathLoc, domSource)).thenReturn("test#XA-DE-TF");
+    when(xpathTemplate.evaluateAsString(xpathName, domSource)).thenReturn("Johann Wolfgang von Goethe");
+    Calendar calB = new GregorianCalendar(1749, 05, 14);
+    Calendar calD = new GregorianCalendar(1832,06,16);
+
+    Node node = mock(Node.class);
+    Node nodeChild = mock(Node.class);
+    node.appendChild(nodeChild);
+    when(nodeChild.getTextContent()).thenReturn("http://d-nb.info/gnd/118540238");
+    when(node.getFirstChild()).thenReturn(nodeChild);
+    List<Node> nodes = new ArrayList<>();
+    nodes.add(node);
+    when(xpathTemplate.evaluateAsNodeList(xpathFacet, domSource)).thenReturn(nodes);
     
     
     String url = "https://api.deutsche-digitale-bibliothek.de/items/itemId/aip?oauth_consumer_key=authkey";
     String url2 = "http://d-nb.info/gnd/118540238/about/rdf";
     String url3 = "http://d-nb.info/gnd/4062257-5/about/rdf";
-    when(rest.getForObject(url, ItemAipResult.class)).thenReturn(result);
+    when(rest.getForObject(url, DOMSource.class)).thenReturn(domSource);
     
-    when(rest.getForObject(url2, DNBAuthorItem.class)).thenReturn(dnbA);
+    when(rest.getForObject(url2, DOMSource.class)).thenReturn(domSource);
 
-    when(rest.getForObject(url3, DNBLocationItem.class)).thenReturn(dnbL);
+    when(rest.getForObject(url3, DOMSource.class)).thenReturn(domSource);
     
     DDBItem ddbItem = fetcher.fetchMetadata("itemId");
     
