abc34549b47954cdcc82cd0ea0a8a6a45c88f17e
LichtiNam <goerickm@zedat.fu-berlin.de>
2015-01-15 20:42:36 +0100

    change search to SearchItem

--- src/test/java/de/ddb/pdc/web/SearchControllerTest.java
+++ src/test/java/de/ddb/pdc/web/SearchControllerTest.java
@@ -1,6 +1,7 @@
 package de.ddb.pdc.web;
 
 
+import de.ddb.pdc.metadata.SearchItem;
 import org.junit.Assert;
 import org.junit.Test;
 import org.mockito.Mockito;
@@ -14,8 +15,9 @@ public class SearchControllerTest {
   public void search() {
     MetaFetcher fetcher = Mockito.mock(MetaFetcher.class);
     DDBItem[] result = new DDBItem[0];
-    Mockito.when(fetcher.searchForItems("foo", 0, 10, "relevance")).thenReturn(result);
+    SearchItem searchItem = new SearchItem(0, result);
+    Mockito.when(fetcher.searchForItems("foo", 0, 10, "relevance")).thenReturn(searchItem);
     SearchController controller = new SearchController(fetcher);
-    Assert.assertSame(result, controller.search("foo", 10, null));
+    Assert.assertSame(searchItem, controller.search("foo", 10, null));
   }
 }
