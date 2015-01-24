1f2800970a0a3014a7922ff6bf9d17191ccb473c
Denis Washington <denis@denisw.de>
2015-01-24 14:10:47 +0100

    Remove unused import

--- src/test/java/de/ddb/pdc/web/PDCControllerTest.java
+++ src/test/java/de/ddb/pdc/web/PDCControllerTest.java
@@ -19,7 +19,6 @@ import org.springframework.boot.test.SpringApplicationConfiguration;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
-import static org.junit.Assume.assumeNotNull;
 import static org.mockito.Mockito.*;
 
 @RunWith(SpringJUnit4ClassRunner.class)
