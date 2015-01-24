b75ed83fb091f718e26a24e2da81ff8795d65c9a
Frank Zechert <admin@ch3ck3r.org>
2015-01-24 14:15:13 +0100

    Country of origin of an item is always assumed to be Germany, as requested by the customer. Closes #24

--- src/main/java/de/ddb/pdc/core/answerers/CountryOfOriginTriptisAnswerer.java
+++ src/main/java/de/ddb/pdc/core/answerers/CountryOfOriginTriptisAnswerer.java
@@ -15,35 +15,11 @@ class CountryOfOriginTriptisAnswerer implements Answerer {
   private String note;
 
   /**
-   * Answer whether the country that the item was created in is a member of the
-   * Berne Triptis WCT.
-   *
-   * FIXME this is mis-using the author-nationality.
+   * Country of origin is always Germany as requested by the customer
    */
   @Override
   public Answer answerQuestionForItem(DDBItem metaData) {
-    // FIXME wrong nationality used here
-    List<Author> authors = metaData.getAuthors();
-    if (authors == null || authors.isEmpty()) {
-      this.note = "Kein(e) Autor(en) bekannt";
-      return Answer.UNKNOWN;
-    }
-
-    boolean result = true;
-    this.note = "Herkunftsland ist eins oder mehrere der folgendenden: ";
-    for (Author author : authors) {
-      if (BerneTriptisMembers.isMember(author.getNationality())) {
-        this.note += author.getNationality() + " (Mitglied), ";
-      } else {
-        this.note += author.getNationality() + " (kein Mitglied), ";
-        result = false;
-      }
-    }
-    this.note = this.note.substring(0, this.note.length() - 2);
-    if (result) {
-      return Answer.YES;
-    }
-    return Answer.NO;
+    return Answer.ASSUMED_YES;
   }
 
   /**
@@ -51,6 +27,7 @@ class CountryOfOriginTriptisAnswerer implements Answerer {
    */
   @Override
   public String getNoteForLastQuestion() {
-    return this.note;
+    return "Es wird immer davon ausgegangen, dass das Herkunftsland des"
+        + " Werkes Deutschland ist.";
   }
 }
